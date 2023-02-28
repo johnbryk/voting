bf714c11e8ec1e9102a9ed7b8cf0269336d7ef84
johnbryk <43798730+johnbryk@users.noreply.github.com>
2023-02-28 17:57:38 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-// axios sends our get and post requests
 import axios from 'axios'
-// handling state and running effects on load/when some values change
 import { useState, useEffect } from 'react'
 import './App.css'
-// I extracted the button to its own component
 import Button from './Button.js'
 
-// both get and post are directed to this url
 const url = '/api/data'
 
-// our app
 const App = () => {
-  // this feels self-explanatory
   const [question, setQuestion] = useState('')
   const [choices, setChoices] = useState([])
-  const [voted, setVoted] = useState(false)
 
-  // totals just extracts the vote total for each choice
+  const voted = localStorage.voted ? localStorage.voted : false
   const totals = choices.map(choice => choice.total)
-  // this is the total number of votes--this just adds the totals together
   const totalVotes = totals.reduce((prev, curr) => prev+curr, 0)
-  // this computes and formats the percent of votes for each choice
   const percents = totals.map(total => (total / totalVotes)
     .toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0}))
 
-  // when we load the page, get the data from the server
-  // then extract the question and choices from the data
-  // we don't use the date here. we could have. I just didn't
   useEffect(() => {
     axios
       .get(url)
@@ -36,31 +24,21 @@ const App = () => {
       })
   }, [])
 
-  // cick handler: sends a post request
-  // the post request is this simple object: {vote: #}
-  // when we get a response, we update choices (which really just updates the vote totals)
-  // and then we set voted as true, which affects what they see/whether they can click the buttons
   const onClick = (i) => {
     axios
       .post(url, {vote: i})
       .then(res => {
         setChoices(res.data.choices)
-        setVoted(true)
+        localStorage.voted = true
       })
   }
 
-  // here's the JSX
   return (
     <div>
-      {/* the question */}
       <div className='question'>
         {question.toUpperCase()}
       </div>
-      {/* the buttons */}
       <div className='buttons'>
-        {/* maps eac choice to a button; the button needs choice for image and text;
-        the callback function does nothing if they've voted; percent is percent;
-        voted is passed so we know how to render the buttons--show percents or not */}
         {choices.map((choice, vote) => (
           <Button key={vote} choice={choice} onClick={voted ? null : () => {onClick(vote)}} percent={percents[vote]} voted={voted}/>
         ))}
--- src/Button.js
+++ src/Button.js
@@ -1,43 +1,26 @@
-// useState and useEffect here are used for the button animation
 import { useState, useEffect } from 'react'
 
-// our button gets some props from the app
 const Button = (props) => {
 
-  // the buttons are now animated to show the percents increasing from 0 to whatever they should be
-  // percentGrow represents this increasing value
   const [percentGrow, setPercentGrow] = useState(0)
 
-  // I put the images into a directory called img in public. this becomes a directory called img in
-  // build. since our server app redirects url requests that are not handled by our explicit get
-  // and post requests, this will get redirected to /build/img/whatever.png
   const img = `/img/${props.choice.img}`
-  
-  // the rest of this is pretty clear I hope
   const txt = props.choice.txt
   const onClick = props.onClick
   const percent = props.percent
   const voted = props.voted
 
-  // this keeps track of whether the animation continues--if the displayed value is less than the actual value,
-  // ... keep growing!
   const keepGrowing = percentGrow < percent.slice(0, percent.length-1)
 
-  // this increments percentGrow--if you voted and percentGrow < pecent, increment percentGrow
   useEffect(() => {
     console.log('happens')
     if(voted && keepGrowing) {
       window.setTimeout(() => { setPercentGrow(percentGrow+1) }, 10)
     }
-    // this updates when props.voted changes (this could probably just be voted) and when percentGrow changes
-    // this is because we want the animation to start when props.voted changes from false to true
-    // and because percentGrow stops changing when it is equal to percent, so this will stop being called
   }, [props.voted, percentGrow])
 
   return (
     <div>
-      {/* the "button" is really a div with the image is background
-      I set an inner shadow over the box if the user has voted */}
       <div
         className='button'
         style={{
@@ -46,8 +29,6 @@ const Button = (props) => {
         }}
         onClick={onClick}
       >
-        {/* if the user voted, show either percentGrow or percent depending on whether percentGrow is still
-        growing or not; if the user didn't vote yet, don't show anything */}
         {voted ? (keepGrowing ? percentGrow + "%" : percent) : ''}
       </div>
       {txt.toUpperCase()}
